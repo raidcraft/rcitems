e6b319a53982ff57ac584f732fa5a8ff534f642d
Silthus <silthus@raid-craft.de>
2013-04-25 16:49:12 +0200

    initial commit of custom items

---
+++ src/main/java/de/raidcraft/items/ItemsPlugin.java
@@ -0,0 +1,19 @@
+package de.raidcraft.items;
+
+import de.raidcraft.api.BasePlugin;
+
+/**
+ * @author Silthus
+ */
+public class ItemsPlugin extends BasePlugin {
+
+    @Override
+    public void enable() {
+        //TODO: implement
+    }
+
+    @Override
+    public void disable() {
+        //TODO: implement
+    }
+}
