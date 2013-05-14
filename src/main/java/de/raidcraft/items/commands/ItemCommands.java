1090d0c2fab1db474748d5e630ba1d600e8a2035
Silthus <silthus@raid-craft.de>
2013-05-14 18:26:12 +0200

    fixed couple of NPE and some bugs

--- src/main/java/de/raidcraft/items/commands/ItemCommands.java
+++ src/main/java/de/raidcraft/items/commands/ItemCommands.java
@@ -3,6 +3,7 @@ package de.raidcraft.items.commands;
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.minecraft.util.commands.CommandPermissions;
+import com.sk89q.minecraft.util.commands.NestedCommand;
 import de.raidcraft.RaidCraft;
 import de.raidcraft.api.items.CustomItemManager;
 import de.raidcraft.api.items.CustomItemStack;
@@ -28,9 +29,23 @@ public class ItemCommands {
             min = 1
     )
     @CommandPermissions("rcitems.give")
+    @NestedCommand(value = SubCommands.class, executeBody = true)
     public void giveItem(CommandContext args, CommandSender sender) {
 
         CustomItemStack itemStack = RaidCraft.getComponent(CustomItemManager.class).getCustomItemStack(args.getInteger(0));
         ((Player) sender).getInventory().addItem(itemStack.getHandle());
+    }
+
+    public class SubCommands {
+
+        @Command(
+                aliases = {"wizard", "config", "new", "create"},
+                desc = "Creates a new Item in the Item Wizard."
+        )
+        @CommandPermissions("rcitems.create")
+        public void wizard(CommandContext args, CommandSender sender) {
+
+
+        }
     }
 }
