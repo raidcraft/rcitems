f673ee8db4329e242e0cf971ac34e1a4848d2382
Silthus <silthus@raid-craft.de>
2013-09-17 18:10:41 +0200

    added column name

--- src/main/java/de/raidcraft/items/tables/items/TItemAttachmentData.java
+++ src/main/java/de/raidcraft/items/tables/items/TItemAttachmentData.java
@@ -2,6 +2,7 @@ package de.raidcraft.items.tables.items;
 
 import de.raidcraft.api.config.KeyValueMap;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -15,6 +16,7 @@ public class TItemAttachmentData implements KeyValueMap {
 
     private int id;
     @ManyToOne
+    @Column(name = "attachment_id")
     private TCustomItemAttachment attachment;
     private String dataKey;
     private String dataValue;
